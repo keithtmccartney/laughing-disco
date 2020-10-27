b00de4efbfa5b80536a2f9bfe4fb35ccade7a30b
Keith <keithtmccartney@hotmail.com>
2020-10-27 09:29:01 +0000

    Adding a new 'image-upload' Component.

--- ImageDragDrop/src/app/app.module.ts
+++ ImageDragDrop/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageUploadComponent } from './image-upload/image-upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageUploadComponent
   ],
   imports: [
     BrowserModule,
